87d4448e36f50c749e83f2e2110aa12cab8ee2c1
Franco Sanchez <francomaximosanchez@gmail.com>
2025-07-10 22:57:47 -0300

    URLs añadidas

--- catasy/urls.py
+++ catasy/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('featured.urls')),
+    path('questions/', include('questions.urls')),
+    path('allgames/', include('allgames.urls')),
+    path('about/', include('about.urls')),
 ]
